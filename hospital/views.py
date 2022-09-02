62e72ab4b02ae9799656f7bc4462832e12728d9d
99Meliodas <a.nurtai1999@gmail.com>
2022-09-02 23:14:03 +0600

    finished patients, index, base

--- hospital/views.py
+++ hospital/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
-
+from .models import *
 def index(request):
-    return render(request, 'index.html')
+    doctors = Doctor.objects.all()
+    nurses = Nurse.objects.all()
+    context = {
+        'doctors': doctors,
+        'nurses': nurses,
+    }
+    return render(request, 'index.html', context=context)
+
+def patients(request):
+    patient = Patient.objects.all()
+    context = {
+        'patients': patient
+    }
+    return render(request, 'patients.html', context=context)
