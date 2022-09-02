bf6c4bd8396e9ae667a5706d96211193dc45a201
LCR497 <abirov999@gmail.com>
2022-09-02 23:04:58 +0600

    Added docDetail.html and logic DoctorDetail

---
+++ hospital/forms.py
@@ -0,0 +1,7 @@
+from .models import *
+from django import forms
+class DoctorDetail(forms.ModelForm):
+
+    class Meta:
+        model = Doctor
+        fields = ('type_dr', 'full_name', 'pin_code', 'age', 'phone_number', 'nurse', 'hospital', )
--- hospital/views.py
+++ hospital/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import *
 def index(request):
     return render(request, 'index.html')
+
+def docDetail(request, pk):
+    doc = get_object_or_404(Doctor, pk=pk)
+    context = {
+        'doctor' : doc
+    }
+    return render(request, 'docdetail.html', context=context)
