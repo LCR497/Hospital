f872c93b9aa16a62375d90b50e2e436848675f0d
LCR497 <abirov999@gmail.com>
2022-09-12 19:08:39 +0600

    Add requirements

--- hospital/urls.py
+++ hospital/urls.py
@@ -4,8 +4,6 @@ from .views import *
 urlpatterns = [
     path('login/',  loginPage, name='login'),
     path("register/", register_request, name="register"),
-    path('api/v1/auth/', include('djoser.urls')),
-    path('api/v1/auth-token/', include('djoser.urls.authtoken')),
     path('',  index, name='index'),
     path('patients/', patients, name='patients'),
     path('docdetail/<int:pk>', docDetail, name='docdetail'),
